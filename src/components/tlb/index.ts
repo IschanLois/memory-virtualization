24258c0c205108287b291b4fc4942588d166b9f4
Christian Olo <christianlois.olo@gmail.com>
2025-01-08 20:54:02 +0800

    refactor: add util for exiting with status code 1

--- src/components/tlb/index.ts
+++ src/components/tlb/index.ts
@@ -1,23 +1,21 @@
+import { errorExit } from '../../utils/errorExit.js'
+
 const expected: string = 'Expected Arguments: -e <entries>'
 
 if (process.argv.length != 4) {
-  console.error(`Invalid number of arguments\n${expected}`)
-  process.exit(1)
+  errorExit(`Invalid number of arguments\n${expected}`)
 }
 
 if (process.argv[2] != '-e') {
-  console.error(`Invalid argument\n${expected}`)
-  process.exit(1)
+  errorExit(`Invalid argument\n${expected}`)
 }
 
 const entries: number = Number(process.argv[3])
 
 if (isNaN(entries)) {
-  console.error(`Invalid number of entries: Must be an integer\n${expected}`)
-  process.exit(1)
+  errorExit(`Invalid number of entries: Must be an integer\n${expected}`)
 }
 
 if (!entries.toString(2).match(/^10+$/)) {
-  console.error(`Invalid number of entries: Must be a power of 2\n${expected}`)
-  process.exit(1)
+  errorExit(`Invalid number of entries: Must be a power of 2\n${expected}`)
 }
---
+++ src/utils/errorExit.ts
@@ -0,0 +1,4 @@
+export const errorExit = (message: string) => {
+  console.error(message)
+  process.exit(1)
+}
